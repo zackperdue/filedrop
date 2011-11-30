6af5fc4a955f13b886831a8298710d3d78c1d163
Zack Perdue <zackperdue@gmail.com>
2011-11-30 13:27:20 -0500

    Added static file serving of module css,js,img

--- classes/controller/filedrop.php
+++ classes/controller/filedrop.php
@@ -12,7 +12,7 @@ class Controller_Filedrop extends Controller_Template
 	
 	public function action_index ()
 	{
-		var_dump(Filedrop::instance());
+		//var_dump(Filedrop::instance());
 		$this->template->page->content = View::factory('filedrop/filedrop');
 	}
 	
@@ -30,6 +30,37 @@ class Controller_Filedrop extends Controller_Template
 	public function action_upload ()
 	{
 		
+	}
+	
+	//Statif File Serving
+	public function action_media()
+	{
+		// Get the file path from the request
+		$file = $this->request->param('file');
+
+		// Find the file extension
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+
+		// Remove the extension from the filename
+		$file = substr($file, 0, -(strlen($ext) + 1));
+
+		if ($file = Kohana::find_file('media/filedrop', $file, $ext))
+		{
+			// Check if the browser sent an "if-none-match: <etag>" header, and tell if the file hasn't changed
+			$this->response->check_cache(sha1($this->request->uri()).filemtime($file), $this->request);
+			
+			// Send the file content as the response
+			$this->response->body(file_get_contents($file));
+
+			// Set the proper headers to allow caching
+			$this->response->headers('content-type',  File::mime_by_ext($ext));
+			$this->response->headers('last-modified', date('r', filemtime($file)));
+		}
+		else
+		{
+			// Return a 404 status
+			$this->response->status(404);
+		}
 	}
 	
 	public function before ()
@@ -52,12 +83,12 @@ class Controller_Filedrop extends Controller_Template
 		if ( $this->auto_render )
 		{
 			$styles = array(
-				'resources/css/style.css' => 'screen',
+				'media/filedrop/css/style.css' => 'screen',
 			);
 			
 			$scripts = array(
-				'resources/js/scripts.js',
-				'resources/js/jquery.filedrop.js',
+				'media/filedrop/js/scripts.js',
+				'media/filedrop/jquery.filedrop.js',
 				'http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js',
 			);
 			
